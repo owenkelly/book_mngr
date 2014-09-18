34e73867335ba6af7c59ca2f49d4a10b9249e052
Owen Kelly <owen.james.kelly@gmail.com>
2014-09-18 16:20:58 -0700

    changes to the email settings

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
     :port =>           '587',
     :address =>        'smtp.mandrillapp.com',
     :user_name =>      	ENV['MANDRILL_USERNAME'],
-    :password =>       	ENV['MANDRILL_APIKEY],
+    :password =>       	ENV['MANDRILL_APIKEY'],
     :domain =>         'heroku.com',
     :authentication => :plain
 }
